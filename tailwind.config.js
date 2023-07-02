ab0c4616db1999b6a078373dc279e81197463697
Tsu <lucanahtsu@gmail.com>
2023-07-02 20:31:15 -0300

    add custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,30 @@ export default {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#A44200",
+
+          secondary: "#F2F3AE",
+
+          accent: "#69140E",
+
+          neutral: "#1c1917",
+
+          "base-100": "#F3F3F3",
+
+          info: "#a1cae3",
+
+          success: "#116937",
+
+          warning: "#f3c95e",
+
+          error: "#ed6e78",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 };
