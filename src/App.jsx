d97f1e7f241a635fc233e405f353747566e5db80
Tsu <lucanahtsu@gmail.com>
2023-07-03 12:40:00 -0300

    add hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Header from './components/Header'
+import HeroSection from './components/HeroSection'
+import ProductSection from './components/ProductSection'
+import Footer from './components/Footer'
+
 function App() {
 
   return (
     <>
-       <Header />
-{/*        
-      <HeroSection />
+      <Header />
+      <HeroSection/>
       <ProductSection />
-      <Footer />  */}
+      <Footer />
     </>
   )
 }
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
 
 function HeroSection() {
-    return(
-    <section id="hero">
-
+  const heroImageURL = "https://plus.unsplash.com/premium_photo-1682390303528-0ef38fd25bfa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80";
+  
+  return (
+    <section id="hero" className="hero min-h-screen bg-center bg-cover bg-fixed bg-black/50" style={{ backgroundImage: `url("${heroImageURL}")` }}>
+      <div className="hero-overlay"></div>
+      <div className="hero-content text-center text-white">
+        <div className="max-w-2xl">
+          <h2 className="mb-5 text-xl sm:text-3xl font-serif fadeIn">¿Qué esperas para tener los mejores mates artesanales del Noroeste Argentino?</h2>
+          <a href="#productos"><button className="btn btn-primary btn-sm md:btn-md">Quiero lo mejor</button></a>
+        </div>
+      </div>
     </section>
-    )
+  );
 }
 
 export default HeroSection;
