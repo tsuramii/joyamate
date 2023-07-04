a6843ca41a906b5f13585cddd1b043e767185b69
Tsu <lucanahtsu@gmail.com>
2023-07-04 18:40:36 -0300

    fix navbar routing error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,13 +10,13 @@ function Header() {
         <nav className="navbar text-base-100 bg-neutral fixed z-10">
            <div className="navbar-start hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a href="hero" className="hover:text-secondary">Inicio</a></li>
+                    <li><a href="#hero" className="hover:text-secondary">Inicio</a></li>
                     <li tabIndex="0">
                         <details>
                             <summary className="hover:text-secondary">Productos</summary>
                             <ul className="p-2 text-neutral">
-                            <li><a href="mates" className="hover:text-primary">Mates</a></li>
-                            <li><a href="joyeria" className="hover:text-primary">Joyeria</a></li>
+                            <li><a href="#mates" className="hover:text-primary">Mates</a></li>
+                            <li><a href="#joyeria" className="hover:text-primary">Joyeria</a></li>
                             </ul>
                         </details>
                     </li>
@@ -31,15 +31,15 @@ function Header() {
                         </svg>
                     </label>
                     <ul tabIndex="0" className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow text-neutral bg-base-100 rounded-box w-52">
-                        <li><a href="hero">Inicio</a></li>
+                        <li><a href="#hero">Inicio</a></li>
                         <li>
-                            <a href="products">Productos</a>
+                            <a href="#products">Productos</a>
                             <ul className="p-2">
-                                <li><a href="mates">Mates</a></li>
-                                <li><a href="joyeria">Joyeria</a></li>
+                                <li><a href="#mates">Mates</a></li>
+                                <li><a href="#joyeria">Joyeria</a></li>
                             </ul>
                         </li>
-                        <li><a href="contact">Contactános</a></li>
+                        <li><a href="#contact">Contactános</a></li>
                     </ul>
                 </div>
                 <a href="#" className=""><img src={logo} alt="joyamate-logo" className="h-[6vh] m-0"></img></a>
